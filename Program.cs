566c041f0751b50b1f4797770a7556c7eed69ba2
Тарас Курцеба <taraskurtseba@gmail.com>
2024-11-07 15:54:18 +0200

    Completing the Data access layer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ShopDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConString")));
+//TODO::1.Add repository and interaces to them
+//TODO::2.Create API
+//TODO::3.Create View using API
+//TODO::4.Create data Validation
+//TODO::5.Create Error handling
 
 var app = builder.Build();
 
